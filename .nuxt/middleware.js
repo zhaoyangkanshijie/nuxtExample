a84f411a1c7fc4c2e43f08542ca6db79a44fbbfa
zhaoyangkanshijie <2191732408@qq.com>
2020-11-12 14:15:39 +0800

    test lifecycle

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['stats'] = require('..\\middleware\\stats.js')
-middleware['stats'] = middleware['stats'].default || middleware['stats']
-
-export default middleware
--- pages/test/home.vue
+++ pages/test/home.vue
@@ -46,6 +46,30 @@ export default Vue.extend({
         }
       ]
     }
+  },
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
   }
 })
 </script>
