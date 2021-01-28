fd002828b93a698afc74e9e83d969b066ddbf69a
zhaoyangkanshijie <2191732408@qq.com>
2021-01-28 17:19:28 +0800

    nuxt

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -14,6 +14,7 @@ import { createStore } from './store.js'
 
 import nuxt_plugin_axios_ad7e9b50 from 'nuxt_plugin_axios_ad7e9b50' // Source: .\\axios.js (mode: 'all')
 import nuxt_plugin_elementui_d905880e from 'nuxt_plugin_elementui_d905880e' // Source: ..\\plugins\\element-ui (mode: 'all')
+import nuxt_plugin_libcomponents_e3339b5c from 'nuxt_plugin_libcomponents_e3339b5c' // Source: ..\\plugins\\lib-components (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -181,6 +182,10 @@ async function createApp (ssrContext) {
 
   if (typeof nuxt_plugin_elementui_d905880e === 'function') {
     await nuxt_plugin_elementui_d905880e(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_libcomponents_e3339b5c === 'function') {
+    await nuxt_plugin_libcomponents_e3339b5c(app.context, inject)
   }
 
   // If server-side, wait for async component to be resolved first
--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         My groundbreaking Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -28,9 +24,14 @@
         home
       </nuxt-link>
       <ul>
-        <li v-for="item in items" :key="item">{{item}}</li>
+        <li v-for="item in items" :key="item">
+          {{ item }}
+        </li>
       </ul>
-      <button @click="arrayChange()">test</button>
+      <button @click="arrayChange()">
+        test
+      </button>
+      <p>{{ result }}</p>
     </div>
   </div>
 </template>
@@ -40,9 +41,9 @@ import Vue, { VNode, PropType } from 'vue'
 import Logo from '~/components/Logo.vue'
 
 interface ComplexMessage {
-  title: string,
-  okMessage: string,
-  cancelMessage: string
+  title: string;
+  okMessage: string;
+  cancelMessage: string;
 }
 
 export default Vue.extend({
@@ -70,10 +71,28 @@ export default Vue.extend({
       default: null
     }
   },
+  async fetch () {
+    const response = await this.$axios.get('https://training.tp-link.com.cn/api/values')
+    const fetchResult = response.data
+    const show = []
+    for (const key in fetchResult) {
+      show.push(key)
+      show.push(fetchResult[key])
+    }
+    this.items = show
+  },
+  async asyncData ({ app }) {
+    // called every time before loading the component
+    const response = await app.$axios.get('https://training.tp-link.com.cn/api/values')
+    const result = response.data
+    return { result }
+  },
   data () {
     return {
       msg: 'Hello',
-      items: ['']
+      items: [''],
+      timer: 0,
+      result: ''
     }
   },
   computed: {
@@ -82,57 +101,61 @@ export default Vue.extend({
       return this.greet() + '!'
     }
   },
+  beforeCreate () {
+    console.log('beforeCreate')
+    // this.$axios.get('https://training.tp-link.com.cn/api/values').then((res) => {
+    //   console.log(res.data)
+    //   const data = res.data
+    //   const show = []
+    //   for (const key in data) {
+    //     show.push(key)
+    //     show.push(data[key])
+    //   }
+    //   console.log(show)
+    //   this.items = show
+    // })
+  },
+  created () {
+    console.log('created')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+    // this.timer = setInterval(function () {
+    //   console.log(new Date())
+    // }, 3000)
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
+    // clearInterval(this.timer)
+  },
   methods: {
     // 需要标注有 `this` 参与运算的返回值类型
     greet (): string {
       return this.msg + ' world'
     },
-    arrayChange(): void {
-      //可以更新
-      //this.items = ['a','b','c'];
+    arrayChange (): void {
+      // 可以更新
+      // this.items = ['a','b','c'];
       // this.items = ['a1','a2','b'].filter((value,index)=>{
       //   return value.indexOf('a') != -1;
       // });
-      //this.items = this.items.slice(1)
-      //this.items = this.items.concat(['a1','b1','c1']);
-      //不可以更新
-      //this.items[1] = 'b';
+      // this.items = this.items.slice(1)
+      // this.items = this.items.concat(['a1','b1','c1']);
+      // 不可以更新
+      // this.items[1] = 'b';
     }
-  },
-  beforeCreate(){
-    console.log('beforeCreate')
-  },
-  created(){
-    console.log('created')
-    this.$axios.get("https://training.tp-link.com.cn/api/values").then(res=>{
-        console.log(res.data)
-        let data = res.data;
-        let show = [];
-        for(let key in data){
-          show.push(key);
-          show.push(data[key]);
-        }
-        console.log(show);
-        this.items = show;
-    })
-  },
-  beforeMount(){
-    console.log('beforeMount')
-  },
-  mounted(){
-    console.log('mounted')
-  },
-  beforeUpdate(){
-    console.log('beforeUpdate')
-  },
-  updated() {
-    console.log('updated')
-  },
-  beforeDestroy() {
-    console.log('beforeDestroy')
-  },
-  destroyed(){
-    console.log('destroyed')
   },
   // `createElement` 是可推导的，但是 `render` 需要返回值类型
   render (createElement): VNode {
@@ -142,5 +165,5 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-  @import "../pageStyle/index.scss";
+@import "../pageStyle/index.scss";
 </style>
